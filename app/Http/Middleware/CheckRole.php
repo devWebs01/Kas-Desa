d59d026ce47e4d7d369062b77fc17db9f5937417
adptra01 <m.a.syaputra.94@gmail.com>
2025-04-12 11:00:51 +0700

    reset

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class CheckRole
 {
@@ -16,7 +15,7 @@ class CheckRole
     public function handle(Request $request, Closure $next, ...$roles)
     {
         // Periksa apakah pengguna memiliki salah satu peran yang diberikan
-        if (!in_array($request->user()->role, $roles)) {
+        if (! in_array($request->user()->role, $roles)) {
             return back()->withErrors('Akun tidak memiliki akses');
         }
 
