f11063d28bab81d45af3dbe75cbdf2c0b48b6317
adptra01 <m.a.syaputra.94@gmail.com>
2025-06-26 01:56:59 +0700

    fix: tampilan reset password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -9,51 +9,47 @@
 
         <input type="hidden" name="token" value="{{ $token }}">
 
-        <div class="row mb-3">
-            <label for="email" class="col-md-4 col-form-label text-md-end">Alamat Email</label>
+        <div class="mb-3">
+            <label for="email" class="form-label text-md-end">Alamat Email</label>
 
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control @error("email") is-invalid @enderror"
-                    name="email" value="{{ $email ?? old("email") }}" required autocomplete="email" autofocus>
+            <input id="email" type="email" class="form-control @error("email") is-invalid @enderror"
+                name="email" value="{{ $email ?? old("email") }}" required autocomplete="email" autofocus>
 
-                @error("email")
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+            @error("email")
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
         </div>
 
-        <div class="row mb-3">
-            <label for="password" class="col-md-4 col-form-label text-md-end">Kata Sandi</label>
+        <div class="mb-3">
+            <label for="password" class="form-label text-md-end">Kata Sandi</label>
 
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control @error("password") is-invalid @enderror"
-                    name="password" required autocomplete="new-password">
+            <input id="password" type="password" class="form-control @error("password") is-invalid @enderror"
+                name="password" required autocomplete="new-password">
 
-                @error("password")
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+            @error("password")
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
         </div>
 
-        <div class="row mb-3">
-            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">Konfirmasi Kata Sandi</label>
+        <div class="mb-3">
+            <label for="password-confirm" class="form-label text-md-end">Konfirmasi Kata Sandi</label>
 
-            <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required
-                    autocomplete="new-password">
-            </div>
+            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required
+                autocomplete="new-password">
+
         </div>
 
-        <div class="row mb-0">
-            <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    Reset Kata Sandi
-                </button>
-            </div>
+        <div class="mb-0">
+            <button type="submit" class="btn btn-primary">
+                Reset Kata Sandi
+            </button>
+
         </div>
     </form>
 </x-auth-layout>
